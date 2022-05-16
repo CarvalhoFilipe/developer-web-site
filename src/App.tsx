302de3cfde2e9c96083f188f63b8a05e817b772f
Ewerson Pulquerio <pulquerio.x@gmail.com>
2022-05-16 14:21:45 -0300

    Comentário adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header";
 import { Router } from "./routes";
 import { BrowserRouter } from "react-router-dom";
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Router />
+      <Footer /> {/*Teste de commit!*/}
     </BrowserRouter>
   );
 }
